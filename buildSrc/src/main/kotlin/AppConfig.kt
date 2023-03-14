7b77fe74ee418440a8325392eb9e653a8ab2d565
Vitor Nascimento <bielvitor2008@hotmail.com>
2023-03-14 20:20:48 -0300

    feat: implement kotlin dsl

---
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -0,0 +1,12 @@
+object AppConfig {
+    const val namespace = "me.vitornascimento.trendingrepositories"
+    const val compileSdk = 33
+    const val applicationId = "me.vitornascimento.trendingrepositories"
+    const val minSdk = 24
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val jvmTarget = "1.8"
+    const val kotlinCompilerExtensionVersion = "1.2.0"
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,31 @@
+object Dependencies {
+
+    object Android {
+        const val coreKtx = "androidx.core:core-ktx:${Versions.Android.coreKtx}"
+        const val lifecycleRuntimeKtx =
+            "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.Android.lifecycleRuntimeKtx}"
+        const val activityCompose =
+            "androidx.activity:activity-compose:${Versions.Android.activityCompose}"
+        const val composeUi = "androidx.compose.ui:ui:${Versions.Android.compose}"
+        const val composePreview =
+            "androidx.compose.ui:ui-tooling-preview:${Versions.Android.compose}"
+        const val material = "androidx.compose.material:material:${Versions.Android.compose}"
+    }
+
+    object Test {
+        const val junit = "junit:junit:${Versions.Test.junit}"
+    }
+
+    object AndroidTest {
+        const val androidJunitExt =
+            "androidx.test.ext:junit:${Versions.AndroidTest.androidJunitExt}"
+        const val espresso = "androidx.test.espresso:espresso-core:${Versions.AndroidTest.espresso}"
+        const val composeJunit = "androidx.compose.ui:ui-test-junit4:${Versions.Android.compose}"
+    }
+
+    object Debug {
+        const val composeUiTooling = "androidx.compose.ui:ui-tooling:${Versions.Android.compose}"
+        const val composeUiTestManifest =
+            "androidx.compose.ui:ui-test-manifest:${Versions.Android.compose}"
+    }
+}
