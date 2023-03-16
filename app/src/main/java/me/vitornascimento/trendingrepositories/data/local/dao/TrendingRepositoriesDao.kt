6e7be8a961ec371a7274f060166d94a4022ec340
Vitor Nascimento <bielvitor2008@hotmail.com>
2023-03-16 15:41:27 -0300

    feat: implement Room database

---
+++ app/src/main/java/me/vitornascimento/trendingrepositories/data/local/dao/TrendingRepositoriesDao.kt
@@ -0,0 +1,22 @@
+package me.vitornascimento.trendingrepositories.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import me.vitornascimento.trendingrepositories.data.local.entity.TrendingRepositoryEntity
+
+@Dao
+interface TrendingRepositoriesDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(movies: List<TrendingRepositoryEntity>)
+
+    @Query("SELECT * FROM trending_repositories ORDER BY stars_count DESC")
+    suspend fun getTrendingRepositories(): List<TrendingRepositoryEntity>
+
+    @Query("SELECT * FROM trending_repositories WHERE page = :page ORDER BY stars_count DESC")
+    suspend fun getTrendingRepositoriesForPage(page: Int): List<TrendingRepositoryEntity>
+
+    @Query("DELETE FROM trending_repositories")
+    suspend fun clearAllTrendingRepositories()
+}
